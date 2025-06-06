bd8ba125c4c90b07f93314ccdcb29b6ddf5ef9a4
Santy_M <santy15021502@gmail.com>
2025-06-06 20:00:27 -0300

    MVP carro

--- Front/client/js/cart.js
+++ Front/client/js/cart.js
@@ -1,40 +1,20 @@
 const box = document.querySelector("#productsBox")
 const listaProductos = []
+localStorage.setItem("carro", "[]")
+let productosPrecios = 0
+let productosContador = 0
 
-const getNeumaticos = () =>{
-    fetch('/Front/objects/neumaticos.json')
-    .then(res =>{
-        return res.json()
-    })
-    .then(productos =>{
-        productos.forEach(element => {
-            listaProductos.push(element)
-            console.log(listaProductos)
-        });
-    }
-)}
-
-const getLlantas = () =>{
-    fetch('/Front/objects/llantas.json')
-    .then(res =>{
-        return res.json()
-    })
-    .then(productos =>{
-        productos.forEach(element => {
-            listaProductos.push(element)
-            console.log(element)
-        });
-    }
-)}
-getLlantas()
-
-const insertProducto = () =>{
-    getNeumaticos()
-    getLlantas()
-    listaProductos.forEach(element => {
-        crearProducto(element)
-    });
+const getProductos = () =>{
+    return localStorage.getItem('carro')
 }
+
+const setProductos = () =>{
+    lista = JSON.parse(getProductos())
+    lista.forEach(element => {
+        productosPrecios += element.precio
+        productosContador += 1
+        crearProducto(element)
+    })};
 
 const crearProducto = (element) =>{
     let product = document.createElement('div')
@@ -44,17 +24,13 @@ const crearProducto = (element) =>{
             <div class="row g-4 align-items-start">
             <div class="col-md-4 text-center">
             <img src="/Front/images/llantas/audi_r18-c451-618950a78f90f7c56616357782879910-640-0.png" class="rounded-circle" alt="Producto" style="width: 140px; height: 140px;">
-            <p class="mt-3 fw-bold">Precio</p>
             </div>
             <div class="col-md-8">
-            <h5 class="card-title">Nombre de producto</h5>
+            <h5 class="card-title display-5">${element.nombre}</h5>
             <ul class="list-unstyled ps-3">
-                <li><strong>Rodado:</strong></li>
-                <li><strong>Marca y modelo: </strong></li>
-                <li><strong>Rango de carga:</strong></li>
-                <li><strong>Alto y ancho:</strong></li>
-                <li><strong>Nombre:</strong></li>
-                <li><strong>Tecnología:</strong></li>
+                <li class="h4 fw-lighter">Marca: ${element.marca}</li>
+                <li class="h4 fw-lighter">Modelo: ${element.modelo}</li>
+                <li class="h4">$${element.precio}</li>
             </ul>
             </div>
             </div> 
@@ -69,6 +45,25 @@ const crearProducto = (element) =>{
             box.appendChild(product)
     }
 
+const ticketBox = document.querySelector(".ticket")
+
+const crearResumen = () =>{
+    ticket = document.createElement('div')
+    ticket.className = "aside-ticket shadow"
+    ticket.innerHTML = `
+          <h5>Resumen</h5>
+          <p>Productos: $${productosPrecios}</p>
+          <p>Impuestos: $${productosContador * 500}</p>
+          <hr class="border-light"/>
+          <p class="fw-bold">Total: $${productosPrecios + productosContador * 500}</p>
+        `
+        if(verificarVacio()){
+            ticket.innerHTML += `<button class="btn btn-light w-100 mt-2" disabled>Generar Ticket</button>`
+        }else{
+             ticket.innerHTML += `<button class="btn btn-light w-100 mt-2">Generar Ticket</button>`
+        }
+    ticketBox.appendChild(ticket)
+    }
 
 
 const verificarVacio = () =>{
@@ -78,7 +73,12 @@ const verificarVacio = () =>{
             No hay productos en el carrito
         </div>
         `
+    }else{
+        return true
     }
 }
 
+setProductos()
+verificarVacio()
+crearResumen()
 
