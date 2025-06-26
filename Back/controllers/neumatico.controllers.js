0115ffd92253d6e21158ad14356bf72a46de51e9
Santy_M <santy15021502@gmail.com>
2025-06-26 19:25:14 -0300

    listo el activar y desactivar

--- Back/controllers/neumatico.controllers.js
+++ Back/controllers/neumatico.controllers.js
@@ -34,7 +34,6 @@ export const putNeumatico = async (req, res) => {
   const actualizado = await neumaticoService.updateNeumatico(req.params.id, req.body);
 
   if (!actualizado) return res.status(404).json({ error: 'Neumático no encontrado' });
-  // Si el neumático no se encuentra, se devuelve un error 404
   res.json(actualizado);
 
 };
