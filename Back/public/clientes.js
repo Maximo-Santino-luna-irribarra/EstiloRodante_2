d1352c988835d8b7ec8fdaaf78f2fe6a43abb61b
Maximo Santino Luna Irribarra <148594458+Maximo-Santino-luna-irribarra@users.noreply.github.com>
2025-07-03 19:11:36 -0300

    logout en todo

--- Back/public/clientes.js
+++ Back/public/clientes.js
@@ -24,3 +24,23 @@ fetch('/api/cliente')
             tbody.innerHTML = '<tr><td colspan="1"> class="text-center text-danger">Error al cargar los datos.</td></tr>';});
 
 
+function logout() {
+const logoutBtn = document.getElementById("LogOut");
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", () => {
+            fetch('/auth/logout', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' }
+                })
+                .then(res => {
+                if (res.ok) {
+                    localStorage.clear()
+                    window.location.href = "/login";
+                } else {
+                    console.error("Error al cerrar sesión");
+                }
+                })
+                .catch(err => console.error("Error en la solicitud de cierre de sesión:", err));
+        });
+}
+}
