4c511c6289838f261ee3c49d33c41ed4d1eb6f56
Maximo Santino Luna Irribarra <148594458+Maximo-Santino-luna-irribarra@users.noreply.github.com>
2025-07-11 11:04:08 -0300

    a

--- Front/client/js/products.js
+++ Front/client/js/products.js
@@ -22,28 +22,7 @@ let productosFiltrados = [];
 let carrito = cargarCarrito();
 let allBrands = [];
 
-// ------------------------------
-// MODO OSCURO
-// ------------------------------
-const modoActual = localStorage.getItem("modo") || "dia";
-if (modoActual === "noche") {
-  document.body.classList.add("dark-mode");
-  modoBtn.textContent = "☀️";
-} else {
-  document.body.classList.remove("dark-mode");
-  modoBtn.textContent = "🌙";
-}
-modoBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("modo", "noche");
-    modoBtn.textContent = "☀️";
-    document.documentElement.style.setProperty('--color-fondo', '#222222');
-  } else {
-    localStorage.setItem("modo", "dia");
-    modoBtn.textContent = "🌙";
-  }
-});
+
 
 // ==============================
 // RENDERIZACIÓN DE PRODUCTOS
