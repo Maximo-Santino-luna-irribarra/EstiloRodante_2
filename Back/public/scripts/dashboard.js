98b95530edc2e320f148c9c4d9aa5a9839868513
Maximo Santino Luna Irribarra <148594458+Maximo-Santino-luna-irribarra@users.noreply.github.com>
2025-07-12 19:02:05 -0300

    cositas logu out

--- Back/public/scripts/dashboard.js
+++ Back/public/scripts/dashboard.js
@@ -1,3 +1,4 @@
+
 // Variables globales
 let currentPage = 1;
 const itemsPerPage = 9;
@@ -212,26 +213,7 @@ function renderPaginacion() {
 }
 
 
-function logout() {
-  const logoutBtn = document.getElementById("LogOut");
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", () => {
-      fetch('/auth/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-      })
-      .then(res => {
-        if (res.ok) {
-          localStorage.clear()
-          window.location.href = "/login";
-        } else {
-          console.error("Error al cerrar sesión");
-        }
-      })
-      .catch(err => console.error("Error en la solicitud de cierre de sesión:", err));
-    });
-  }
-}
+
 
 // Inicialización
 inicializarFiltros();
